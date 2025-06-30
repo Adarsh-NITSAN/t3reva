ea585a1eda40818d09ad1011f1154d4ec1cae32a
adarshsharma.nitsan <adarshsharma.nitsan@gmail.com>
2025-06-30 16:59:46 +0530

    [TASK] Updated next js config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')()
+const withNextIntl = require('next-intl/plugin')();
 
 const nextConfig = {
   eslint: {
@@ -9,25 +9,32 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 't3-reva.ddev.site',
+        hostname: 't3reva-v13.ddev.site',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 't3reva.thebetaspace.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 't3-reva.t3planet.com',
+        hostname: 't3reva-v13.thebetaspace.com',
         port: '',
         pathname: '/**',
       },
     ],
     unoptimized: true,
   },
-}
+  i18n: {
+    locales: ['en', 'de'], // Adjust these based on your actual locales
+    defaultLocale: 'en',
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = withNextIntl(nextConfig)
+module.exports = withNextIntl(nextConfig);
